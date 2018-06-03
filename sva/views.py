7248579d909f20724a8b22ff5e107a848d83e2df
Gustavo <gustavohsborba@gmail.com>
2018-06-03 17:23:59 -0300

    adiciona import da função reverse

--- sva/views.py
+++ sva/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from typing import Dict, Any, Union
 
+from django.urls import reverse
 from django.contrib import messages
 from django.db.models.functions import Concat
 from django.utils.translation import gettext_lazy as _
